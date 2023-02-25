0f6ed3a9da4b6a3a4f16d4b0a6531e5b54a907e2
valteem <val.teem.scn@gmail.com>
2023-02-25 16:17:19 +0300

    resolved relative import issue, did some minor housekeeping

---
+++ examples/example-factorial.py
@@ -0,0 +1,14 @@
+import pdb
+# Running from top level package directory:
+# $ python3 -m examples.example-factorial
+# 'It is an antipattern running a script directly from its folder' https://stackoverflow.com/a/68315950
+from algpy.recursion.factorial.factorial import factorial
+
+def example_factorial():
+    print("factorial (5) = " + str(factorial(5)))
+
+#pdb.set_trace()
+
+if __name__ == "__main__":
+  example_factorial()
+  print(str(factorial(7)))
