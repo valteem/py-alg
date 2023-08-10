b63bdef2427a876ae9d5060ee077d3053b4b4243
valteem <val.teem.scn@gmail.com>
2023-08-10 19:46:08 +0300

    comment out prints in bubble sort

--- algpy/sorting/bubble_sort.py
+++ algpy/sorting/bubble_sort.py
@@ -2,12 +2,12 @@ def bubble_sort(list_to_sort):
     n = len(list_to_sort)
     flag_already_sorted = True
     for k in range(n):
-        print("k =", k)
+#        print("k =", k)
         for j in range(n - k - 1):
-            print("j =", j)
+#            print("j =", j)
             if list_to_sort[j] > list_to_sort[j+1]:
                 list_to_sort[j],list_to_sort[j+1] = list_to_sort[j+1],list_to_sort[j]
-                print(list_to_sort)
+#                print(list_to_sort)
                 flag_already_sorted = False
         if flag_already_sorted:
             break
