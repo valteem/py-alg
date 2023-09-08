def6e424cc3633d378eb848bc718f673719b3765
valteem <val.teem.scn@gmail.com>
2023-09-08 20:15:03 +0300

    extended Stack implementation and tests

--- algpy/sequences/stack.py
+++ algpy/sequences/stack.py
@@ -7,6 +7,12 @@ class Stack:
     def is_empty(self) -> bool:
         return len(self._arr) == 0
     def top(self):
-        if self.is_empty:
+        if self.is_empty():
             raise EmptyStack("stack is empty")
         return self._arr[-1]
+    def push(self, e) -> None:
+        self._arr.append(e)
+    def pop(self) -> any:
+            if self.is_empty():
+                raise EmptyStack("stack is empty")
+            return self._arr.pop()
--- algpy/tests/sequences/test_stack.py
+++ algpy/tests/sequences/test_stack.py
@@ -1,9 +1,19 @@
 from algpy import Stack
 from algpy import EmptyStack
+import pytest
 
 def test_stack():
+    
     s = Stack()
-    try:
+    
+    with pytest.raises(Exception) as exc:
         e = s.top()
-    except EmptyStack:
-        print("empty stack")
+    assert str(exc.value) == "stack is empty"
+
+    s.push("e1")
+    assert s.top() == "e1"
+    s.push("e2")
+    assert s.top() == "e2"
+    e = s.pop()
+    assert e == "e2"
+    assert s.top()  == "e1"
