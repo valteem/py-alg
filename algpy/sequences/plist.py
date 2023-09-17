5851ea1ce5aea10af666f52201e9f0102c5ad6c5
valteem <val.teem.scn@gmail.com>
2023-09-17 11:55:36 +0300

    positional list: access and insertion methods

--- algpy/sequences/plist.py
+++ algpy/sequences/plist.py
@@ -3,16 +3,18 @@
 from .ilist import IList
 
 class PList(IList):
+
     class Position:
         def __init__(self, cont, node) -> None:
             self._cont = cont # 'container' - list to which this position belongs
             self._node = node
         def elt(self):
-            return self._node.elt()
+            return self._node._elt
         def __eq__(self, other) -> bool:
             return type(other) is type(self) and other._node is self._node # 'is' used for identity, '==' for equality
         def __ne__(self, other) -> bool:
             return (self == other)
+        
     def _validate(self, pos):
         if not isinstance(pos, self.Position):
             raise TypeError("pos must be of type Position")
@@ -21,14 +23,40 @@ class PList(IList):
         if pos._node._next == None:
             raise ValueError("pos is removed") # underlying IList setx _next and _prev to None for removed nodes 
         return pos._node
+    
     def _position(self, node):
         if node is self._head or node is self._tail:
             return None
         return self.Position(self, node)
+    
+    # access to nodes via their positions in list
+    def head(self):
+        return self._position(self._head._prev)
+    def tail(self):
+        return self._position(self._tail._next)
+    def after(self, pos):
+        n = self._validate(pos)
+        return self._position(n._next)
+    def before(self, pos):
+        n = self._validate(pos)
+        return self._position(n._prev)
+    def __iter__(self):
+        c = self.tail()        # cursor (position)
+        while c is not None:   # reaching head sentinel
+            yield c.elt()      # return value to caller functions, wait for next call at the same place
+            c = self.after(c)
+    
+    # adding nodes to list, return position of added nodes
     def insert_between(self, elt, prev, next):
         node = super().insert(elt, prev, next)
         return self._position(node)
     def insert_to_head(self, elt):
         return self.insert_between(elt, self._head._prev, self._head)
-    def head(self):
-        return self._position(self._head._prev)
+    def insert_to_tail(self, elt):
+        return self.insert_between(elt, self._tail, self._tail._next)
+    def insert_before(self, elt, pos):
+        n = self._validate(pos)
+        return self.insert_between(elt, n._prev, n)
+    def insert_after(self, elt, pos):
+        n = self._validate(pos)
+        return self.insert_between(elt, n, n._next)
--- algpy/tests/sequences/test_plist.py
+++ algpy/tests/sequences/test_plist.py
@@ -2,7 +2,7 @@ from algpy import PList
 
 import pytest
 
-def test_plist():
+def test_pos():
 
     p = PList()
     p.insert_to_head("apples")
@@ -11,3 +11,44 @@ def test_plist():
 
     assert u == v # works
 #    assert u is v # error
+
+def test_insert():
+
+    p = PList()
+    cabbage = p.insert_to_tail("cabbage")
+    onion = p.insert_after("onion", cabbage)
+    p.insert_before("potato", onion)
+
+    assert p.__str__() == "tail<->cabbage<->potato<->onion<->head"
+
+
+def test_access():
+
+    p = PList()
+
+    apple = p.insert_to_head("apple")
+    assert p.head().elt() == "apple"
+
+    cherry = p.insert_before("cherry", apple)
+    assert p.before(apple).elt() == "cherry"
+
+    p.insert_after("pear", cherry)
+    assert p.before(apple).elt() == p.after(cherry).elt() == "pear"
+
+
+def test_iter():
+
+    p = PList()
+
+    cap = 10
+
+    for i in range(cap):
+        if p.is_empty():
+            c = p.insert_to_tail(i)
+        else:
+            c = p.insert_after(i, c)
+    
+    i = 0
+    for e in p:
+        assert e == i
+        i += 1
